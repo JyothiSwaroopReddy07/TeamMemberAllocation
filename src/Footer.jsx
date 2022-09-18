26086cbc427704357a2811bcb99f6b0d4c621b61
Jyothi Swaroop Reddy <107217455+JyothiSwaroopReddy07@users.noreply.github.com>
2022-09-18 10:12:14 +0000

    Initial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="container mt-5">
+        <h6>App made By </h6>
+        <h1>Jyothi Swaroop Reddy</h1>
+    </footer>
+  )
+}
+export default Footer
---
+++ src/NotFound.jsx
@@ -0,0 +1,6 @@
+const NotFound = () => {
+  return (
+    <h1 clasName="text-danger">The searched URL couldnot be found</h1>
+  )
+}
+export default NotFound
